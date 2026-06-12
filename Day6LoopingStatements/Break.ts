d934eb20d97b88cdaa9057370e0881f3ae542a37
Rahul Barik <7r.barik@gmail.com>
2026-06-12 16:32:38 +0530

    learning: Practice some break  statement program

---
+++ Day6LoopingStatements/Break.ts
@@ -0,0 +1,10 @@
+//break statement
+
+for(let i:number=1;i<=10;i++)
+{   
+    console.log(i);
+    if(i==5)
+    {
+        break;   //whenever i value become 5 it will go out of for loop
+    }
+}
