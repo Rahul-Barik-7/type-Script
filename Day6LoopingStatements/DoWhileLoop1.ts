173335351e63fe71d839299111186099f909015a
Rahul Barik <7r.barik@gmail.com>
2026-06-12 16:20:49 +0530

    Practice some  do while loop programs

--- Day6LoopingStatements/DoWhileLoop1.ts
+++ Day6LoopingStatements/DoWhileLoop1.ts
@@ -1,1 +1,13 @@
-// Do While Loop : it execute at least once 
+// Do While Loop : it execute at least once before execution the condition
+
+//Example-1
+ let i:number=10;
+// do{
+//     console.log(i)
+//     i++;
+// }while(i<=10)
+
+do{
+    console.log(i)
+    i--;
+}while(i>=1)
