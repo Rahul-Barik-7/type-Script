8ef137204b1922e6aec85fb9c3acca58e97c12cf
Rahul Barik <7r.barik@gmail.com>
2026-06-12 16:31:34 +0530

    learning: Practice some  continue statement program

---
+++ Day6LoopingStatements/Continue.ts
@@ -0,0 +1,11 @@
+//continue statement
+
+for(let j=1;j<=10;j++)
+{
+    if(j==5 || j==2 || j==10)
+    {
+        continue;  //continue will skip 5 , 2 & 10 then it wil print rest of the numbers 
+    }
+    console.log(j)
+}
+
