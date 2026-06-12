703f47faf8b671f196365eb3b1136bda6a26a06c
Rahul Barik <7r.barik@gmail.com>
2026-06-12 19:08:47 +0530

    Learning: Learned some Named function concept(params , rest params , without params ,function  return type )

---
+++ Day7Functions/NamedFunction.ts
@@ -0,0 +1,34 @@
+// Named Function: A function that is declare with a Name
+/* 
+function functionName(parameter): returntype{
+    //block of code 
+}
+
+functionName(); //calling the function or invoking the function
+*/
+
+//Example-1 (named function with no param and no return types)
+function display(): void {
+    console.log("Welcome")
+}
+display(); //calling function
+
+
+//Example-2 (named function with param and return the types)
+function addNumbers(x: number, y: number): number {
+    return x + y;
+}
+let sum = addNumbers(200, 1);
+console.log(sum);
+
+//Example-3 (rest params)
+//rest parameters restricts the number of values that you can pass to a function
+
+let sumOfNumbers: number = 0
+function addNums(...nums: number[]) {
+    for (let i = 0; i < nums.length; i++) {
+        sumOfNumbers = sumOfNumbers + nums[i]
+    }
+    console.log(`Sum of Numbers is: ${sumOfNumbers}`)
+}
+addNums(10, 20, 30, 50, 30,60);
