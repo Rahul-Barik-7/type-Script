f4cde8a9535e9ceee9c39060796ded8c4e6bc80e
Rahul Barik <7r.barik@gmail.com>
2026-06-15 19:38:08 +0530

    style: reformat codes with prettier

--- Day7Functions1/NamedFunction3.ts
+++ Day7Functions1/NamedFunction3.ts
@@ -1,7 +1,7 @@
 //Named function with default params
-function calculateDiscount(price:number,rate:number=0.50):void{  //passing some default value to paramater
-    let discount=price*rate;
+function calculateDiscount(price: number, rate: number = 0.50): void {  //passing some default value to paramater
+    let discount = price * rate;
     console.log(`Discount Amount: ${discount}`);
 }
 calculateDiscount(1000); // 1000*0.50
-calculateDiscount(1000,0.30); //1000*0.30
+calculateDiscount(1000, 0.30); //1000*0.30
