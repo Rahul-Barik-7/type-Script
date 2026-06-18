04b7efcc005010b7195a95f6c7c492d85b2dfa19
Rahul <7r.barik@gmail.com>
2026-06-19 01:09:42 +0530

    Learning: Practice some more array topics

--- Day9Array/Array2.ts
+++ Day9Array/Array2.ts
@@ -1,16 +1,50 @@
-console.log("=======================WELCOME TO ARRAY=======================")
 //Passing an array variable to the function
 
 
 //Example-1(Search an element in an arrays using function)
 //let empNums: Array<number> = [30, 23, 40];
+console.log("=======================Example-1=======================")
 let arr: number[] = [30, 23, 40];
-function search (ele: number, arr: number []): boolean {
-    for(let i=0; i<arr.length; i++) {
+function search(ele: number, arr: number[]): boolean {
+    for (let i = 0; i < arr.length; i++) {
         if (ele === arr[i]) {
             return true;  //element found 
         }
     }
     return false;
 }
-console.log(search(40,arr));        
+console.log(search(40, arr));
+
+
+
+//Example-2(Search an element in an arrays using function)
+console.log("=======================Example-2=======================")
+let customerNames: Array<string> = ["Rahcit", "Dhooni", "Hardhik"];
+function searchNames(searchElement: string, customerNames: string[]): boolean {
+    for (let j: number = 0; j < customerNames.length; j++) {
+        if (searchElement === customerNames[j]) {
+            return true;
+        }
+    }
+    return false;
+}
+let isPresent = searchNames("Dhoon", customerNames);
+console.log(isPresent);
+
+
+
+//Example-3 (A function takes the array and return the array)
+console.log("=======================Example-3=======================")
+
+function convertUpper(lowerChars: string[]): string[] {
+
+    let upercaseCharacters: Array<string> = [];
+
+    for (let k: number = 0; k < lowerChars.length; k++) {
+        upercaseCharacters[k] = lowerChars[k].toUpperCase();
+    }
+    return upercaseCharacters;
+}
+let lowerChars: Array<string> = ["Java", "Python", "Type-Script"];
+console.log(convertUpper(lowerChars));
+
---
+++ Day9Array/Tuple1.ts
@@ -0,0 +1,7 @@
+    /**
+ * Tuples in TypeScript:
+ * A tuple is a fixed-length array where each element has a specific type.
+ * It helps in storing multiple fields of different data types together.
+ */
+
+//Example-1 (Tuple with 2 values )
