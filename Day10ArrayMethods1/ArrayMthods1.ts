4913ac077cd2954cf1dc92ba8cd8d0cfee275afc
Rahul Barik <7r.barik@gmail.com>
2026-06-25 01:38:26 +0530

    Pratice some Array function concept

---
+++ Day10ArrayMethods1/ArrayMthods1.ts
@@ -0,0 +1,93 @@
+let numbers: number[] = [1, 2, 3, 4, 5];
+let fruits: string[] = [`Apple`, "Banana", 'Mango', "Orange"];
+
+console.log("==========printing arrays values==========");
+console.log("Numbers Array :", numbers);
+console.log("Strings Array :", fruits);
+
+//lenght is a attribue not a method
+console.log("")
+console.log("==========printing arrays length==========");
+console.log(numbers.length);
+console.log(fruits.length);
+
+//method-1 (push)
+//add single or multiple elements to the end of an array
+//syntax- array.push(element1,element2........,elementN)
+console.log("")
+console.log("==========push method==========");
+numbers.push(6);
+console.log("Updated Numbers Array", numbers);
+numbers.push(6, 7, 8);
+console.log("Updated Numbers Array", numbers)
+
+//method-2 (pop)
+//remove the last element from an array and we can capture it by storing that value in to a varible
+//syntax- array.pop();
+console.log("")
+console.log("==========pop method==========");
+let removeElement = fruits.pop();
+console.log("Updated Fruits Array : ", fruits)
+console.log("Removed Element : ", removeElement);
+
+
+//method-3 (shift)
+//remove the first  element from an array and we can capture it by string that value in to a varible
+//syntax- array.shift();
+console.log("")
+console.log("==========shift method==========");
+let firstNumber = numbers.shift();
+console.log("Updated number array : ", numbers);
+console.log("Removed Element : ", firstNumber);
+
+
+//method-4 (unshift)
+//add single or multiple elements to the begining  of an array
+//syntax- array.unshift();
+console.log("")
+console.log("==========unshift method==========");
+fruits.unshift("Kiwi", "Water-melon");
+console.log("Updated Fruits list", fruits);
+
+
+//method-5 (concat)
+//combine two or more arrays
+//syntax- array.concat();
+console.log("")
+console.log("==========concatinated method==========");
+let concatArrayes=numbers.concat([1, 9],[10,11]);
+console.log("concatinated Arrays: ",concatArrayes)
+
+//method-6 (slice)
+//Exract a section of an array
+//ending index will be exclussive, (e.g. if 3 is ending index it will consider 2 (3 - 1 = 2))
+//syntax- array.slice(start, end);
+//exclussive means last provided value will not print (3-1=2) then 2nd index value will be printed
+console.log("")
+console.log("==========slice method==========");
+let extractedArray = fruits.slice(1,4);
+console.log("Afer slice: ", extractedArray);
+
+
+//method-6 (splice)
+//add or remove elements from an array (from anywhere) 
+//syntax- array.splice(start, deleteCount, item1, ......, itemN);
+//Example-1
+console.log("")
+console.log("==========splice method (Example-1) ==========");
+console.log("Current Elements in fruits array: ", fruits);
+let removedElements = fruits.splice(1,3);
+console.log("Updated Fruits Array: After splice(1,3) : ", fruits);
+console.log("Removed Elements After Slice(1,3) : ", removedElements); //(1-index starting from , 3- removing 3 elements from 1st index)
+
+//Example-2
+console.log("==========splice method (Example-1) ==========");
+console.log(fruits);
+fruits.splice(1, 0, "Pineapple", "Grapes");  //(1-from 1st Index, 0-we are not removing any elements, "Pineapple", "Grapes"- newly added elements)
+console.log("After splice(1, 0, 'Pineapple', 'Grapes') : ", fruits);
+
+
+
+
+
+
