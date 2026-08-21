8bad7e7178c33fab70a02740a5a7ab32abaacd02
Rahul Barik <7r.barik@gmail.com>
2026-08-21 20:08:48 +0530

    Revising Object creation using class concept

--- Day13objects/Object4.ts
+++ Day13objects/Object4.ts
@@ -1,6 +1,8 @@
-//(4) : using the classes (JS ES16/TS)
+//(4) : using the classes (JS ES16/TS) - class is a collection of peoperties and methods
 
 class EmployeeDetails {
+
+    //the time of creating the class all the key value pair should be separated by semi-colon
     empFirstName: string;
     empLastName: string;
 
@@ -9,19 +11,22 @@ class EmployeeDetails {
         this.empFirstName = empFirstName;
         this.empLastName = empLastName;
     }
-
-    getEmpInfo(): string {
-        return `${this.empFirstName} ${this.empLastName}`;
-    }
-
+    
     getEmpFullName(): string {
-        return `${this.getEmpInfo()}`
+
+        return `${this.empFirstName} ${this.empLastName}`; 
     }
+    getEmpInfo(): string {
+        return `${this.getEmpFullName()}`
+    }
+
 }
 
 let emp1 = new EmployeeDetails("Rajashree", "jena");
-let emp2 = new EmployeeDetails("Sunny", "Kumar");
-let emp3 = new EmployeeDetails("Abhijit", "Kumar");
+let emp2 = new EmployeeDetails("Rahul", "Barik");
 console.log(emp1);
 console.log(emp1.getEmpInfo());
+console.log(emp2.getEmpInfo());
+console.log(emp1.getEmpFullName());
 
+//* Every objects accures there own varibles and methods but every objects derived from Same class
